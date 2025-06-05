9234cd7229ae63e91c0852914f61056c4b63f937
Shreyaskr1409 <shreyaskr.0914@gmail.com>
2025-06-05 22:58:25 +0530

    addition, subtraction and stack ptr related instruction added

--- cpu/instructions.c
+++ cpu/instructions.c
@@ -2,6 +2,35 @@
 #include "cpu.h"
 #include <stdint.h>
 
+// ADD WITH CARRY
+// Here we need to handle overflow cases and negative cases
+// A = Acc. most significant bit (+ve or -ve)
+// B = second number
+// R = result
+// V = overflow flag
+// TRUTH TABLE:
+// A M R | V
+// 0 0 0 | 0
+// 0 0 1 | 1
+// 0 1 0 | 0
+// 0 1 1 | 0
+// 1 0 0 | 0
+// 1 0 1 | 0
+// 1 1 0 | 1
+// 1 1 1 | 0
+// upon making K-Maps or boolean algebra, we have V = ~(A^M)&(A^R)
+uint8_t ADC(cpu6502 *cpu) {
+    CpuFetchFromBus(cpu);
+    cpu->temp = (uint16_t)cpu->a + (uint16_t)cpu->fetched + (uint16_t)CpuGetFlag(cpu, C);
+    CpuSetFlag(cpu, C, cpu->temp > 255);
+    CpuSetFlag(cpu, Z, (cpu->temp & 0x00FF) == 0);
+    CpuSetFlag(cpu, N, (cpu->temp & 0x80));
+    CpuSetFlag(cpu, V, (~((uint16_t)cpu->a ^ (uint16_t)cpu->fetched)
+                & ((uint16_t)cpu->a ^ (uint16_t)cpu->temp)) & 0x0080);
+    cpu->a = cpu->temp & 0x00FF;
+    return 1;
+}
+
 // AND ACCUMULATOR
 uint8_t AND(cpu6502 *cpu) {
     CpuFetchFromBus(cpu);
@@ -145,4 +174,36 @@ uint8_t CLI(cpu6502 *cpu) {
 uint8_t CLV(cpu6502 *cpu) {
     CpuSetFlag(cpu, I, false);
     return 0;
+}
+
+// Put into stack from accumulator
+uint8_t PHA(cpu6502 *cpu) {
+    // Stackpointer is always an offset to the memory location of 0x0100
+    CpuWriteFromBus(cpu, 0x0100 + cpu->stkp, cpu->a);
+    cpu->stkp--;
+    return 0;
+}
+
+// Take out from stack into accumulator
+uint8_t PLA(cpu6502 *cpu) {
+    cpu->stkp++;
+    cpu->a = CpuReadFromBus(cpu, 0x0100 + cpu->stkp);
+    CpuSetFlag(cpu, Z, cpu->a==0x00);
+    CpuSetFlag(cpu, N, cpu->a&0x80);
+    return 0;
+}
+
+// SUBTRACT WITH CARRY (Borrow)
+uint8_t SBC(cpu6502 *cpu) {
+    CpuFetchFromBus(cpu);
+    uint16_t value = ((uint16_t)cpu->fetched) ^ 0x00FF;
+
+    cpu->temp = (uint16_t)cpu->a + value + (uint16_t)CpuGetFlag(cpu, C);
+    CpuSetFlag(cpu, C, cpu->temp & 0xFF00);
+    CpuSetFlag(cpu, Z, (cpu->temp & 0x00FF) == 0);
+    CpuSetFlag(cpu, N, (cpu->temp & 0x80));
+    CpuSetFlag(cpu, V, (((uint16_t)cpu->a ^ value)
+                & ((uint16_t)cpu->a ^ (uint16_t)cpu->temp)) & 0x0080);
+    cpu->a = cpu->temp & 0x00FF;
+    return 1;
 }
