7394d925c0477730edd9d94decea179b653350b9
Shreyas Kumar <shreyaskr.0914@gmail.com>
2025-06-12 20:29:11 +0530

    Removed prior tests, new tests will be added

--- cpu/cpu_test.h
+++
@@ -1,8 +0,0 @@
-#include "cpu.h"
-#include "../bus/bus.h"
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-void print_ram_range(Bus *bus, uint16_t start_address, int num_bytes);
-void loadNestestRom(Bus *bus, const char* filename);
